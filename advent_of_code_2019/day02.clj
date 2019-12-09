b6f1c0ccb9b62a5e85aa60ffa3cda48807366fb2
James Matanle <jkmatanle@gmail.com>
2019-12-09 15:22:28 -0500

    day05

--- advent_of_code_2019/day02.clj
+++ advent_of_code_2019/day02.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as string]
             [clojure.edn :as edn]))
 
-(defn- p [x] (println x) x)
+(defn- p [x] (clojure.pprint/pprint x) x)
 
 (def input
   "https://adventofcode.com/2019/day/2/input"
@@ -11,46 +11,45 @@
 (def input-fixed
   "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,2,23,13,27,1,10,27,31,2,31,6,35,1,5,35,39,1,39,10,43,2,9,43,47,1,47,5,51,2,51,9,55,1,13,55,59,1,13,59,63,1,6,63,67,2,13,67,71,1,10,71,75,2,13,75,79,1,5,79,83,2,83,9,87,2,87,13,91,1,91,5,95,2,9,95,99,1,99,5,103,1,2,103,107,1,10,107,0,99,2,14,0,0")
 
-(defn operate!
-  [memory-atom i f]
-  (swap! memory-atom
-         assoc
-         (@memory-atom (+ i 3))
-         (f (@memory-atom (@memory-atom (+ i 1)))
-            (@memory-atom (@memory-atom (+ i 2))))))
+(defn arithmetic
+  [memory i f]
+  (assoc memory
+         (memory (+ i 3))
+         (f (memory (memory (+ i 1)))
+            (memory (memory (+ i 2))))))
 
-(defn execute-program!
-  [memory-atom]
-  ((fn [i]
-     (if (= i (dec (count @memory-atom)))
-       memory-atom
-       (cond
-         (= 1 (@memory-atom i)) (do (operate! memory-atom i +)
-                                    (recur (+ i 4)))
-         (= 2 (@memory-atom i)) (do (operate! memory-atom i *)
-                                    (recur (+ i 4)))
-         (= 99 (@memory-atom i)) "done"
-         :else (throw (ex-info {:msg "bad data" :i i})))))
-   0))
+(defn execute
+  "NOTE can tail call optimize... not necessary though..."
+  ([memory]
+   (execute memory 0))
+  ([memory pc]
+   (cond
+     (= 1 (memory pc)) (execute (arithmetic memory pc +)
+                                (+ pc 4))
+     (= 2 (memory pc)) (execute (arithmetic memory pc *)
+                                (+ pc 4))
+     (= 99 (memory pc)) memory
+     :else (throw (ex-info "bad opcode" {})))))
 
-(defn parse-input
+(defn parse-memory-string
   [input]
   (->> (string/split input #",")
        (map edn/read-string)
        (vec)))
 
-(defn parse-and-eval
-  [input]
-  (let [res-atom (atom (parse-input input))]
-    (execute-program! res-atom)
-    (string/join "," @res-atom)))
+(defn f
+  [memorystr]
+  (->> memorystr
+       (parse-memory-string)
+       (execute)
+       (string/join ",")))
 
-(parse-and-eval "1,0,0,0,99") ;; => 2,0,0,0,99 (1 + 1 = 2).
-(parse-and-eval "2,3,0,3,99") ;; => 2,3,0,6,99 (3 * 2 = 6).
-(parse-and-eval "2,4,4,5,99,0") ;; => 2,4,4,5,99,9801 (99 * 99 = 9801).
-(parse-and-eval "1,1,1,4,99,5,6,0,99") ;; => 30,1,1,4,2,5,6,0,99.
+(f "1,0,0,0,99") ;; => 2,0,0,0,99 (1 + 1 = 2).
+(f "2,3,0,3,99") ;; => 2,3,0,6,99 (3 * 2 = 6).
+(f "2,4,4,5,99,0") ;; => 2,4,4,5,99,9801 (99 * 99 = 9801).
+(f "1,1,1,4,99,5,6,0,99") ;; => 30,1,1,4,2,5,6,0,99.
 
-(parse-and-eval input-fixed)
+(f input-fixed)
 ;; => "3895705,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,36,1,19,5,37,2,23,13,185,1,10,27,189,2,31,6,378,1,5,35,379,1,39,10,383,2,9,43,1149,1,47,5,1150,2,51,9,3450,1,13,55,3455,1,13,59,3460,1,6,63,3462,2,13,67,17310,1,10,71,17314,2,13,75,86570,1,5,79,86571,2,83,9,259713,2,87,13,1298565,1,91,5,1298566,2,9,95,3895698,1,99,5,3895699,1,2,103,3895701,1,10,107,0,99,2,14,0,0"
 
 ;; correct
@@ -62,35 +61,41 @@
 ;; What values at addresses 1 and 2 (from 0-99) produce target 19690720 in address 0?
 ;;;;;;;;;;;;
 
-;; backtrack: if memory location 0 exceeds target 19690720, try a different pair. backtrack after executing every instruction.
+(defn execute-2
+  "NOTE can tail call optimize... not necessary though..."
+  ([memory target]
+   (execute-2 memory target 0))
+  ([memory target pc]
+   ((fn [memory pc]
+      (cond
+        (< target (memory 0)) memory ; can backtrack if exceeds target because operations can only increase value.
+        (= 1 (memory pc)) (execute (arithmetic memory pc +)
+                                   (+ pc 4))
+        (= 2 (memory pc)) (execute (arithmetic memory pc *)
+                                   (+ pc 4))
+        (= 99 (memory pc)) memory
+        :else (throw (ex-info "bad opcode" {}))))
+    memory pc)))
 
-(defn execute-program-2!
-  [memory-atom target]
-  ((fn [i]
-     (if (= i (dec (count @memory-atom)))
-       memory-atom
-       (cond
-         (< target (@memory-atom 0)) "value too large" ; backtrack
-         (= 1 (@memory-atom i)) (do (operate! memory-atom i +)
-                                    (recur (+ i 4)))
-         (= 2 (@memory-atom i)) (do (operate! memory-atom i *)
-                                    (recur (+ i 4)))
-         (= 99 (@memory-atom i)) "done"
-         :else (throw (ex-info "bad input data" {})))))
-   0))
+(defn cartesian-product
+  [xs ys]
+  (for [x xs y ys]
+    [x y]))
 
-(defn part-02
-  [program target]
-  (let [res-atom (atom [nil])]
-    (last (for [i (range 100)
-                j (range 100)
-                :while (not= target (@res-atom 0))]
-            (do (reset! res-atom (parse-input program))
-                (swap! res-atom assoc 1 i 2 j)
-                (execute-program-2! res-atom target))))
-    @res-atom))
+(defn make-candidate
+  [memory [x y]]
+  (assoc memory 1 x 2 y))
 
-(part-02 input 19690720)
-;; => [19690720 64 17 2 1 1 2 3 1 3 4 3 1 5 0 3 2 1 9 192 1 19 5 193 2 23 13 965 1 10 27 969 2 31 6 1938 1 5 35 1939 1 39 10 1943 2 9 43 5829 1 47 5 5830 2 51 9 17490 1 13 55 17495 1 13 59 17500 1 6 63 17502 2 13 67 87510 1 10 71 87514 2 13 75 437570 1 5 79 437571 2 83 9 1312713 2 87 13 6563565 1 91 5 6563566 2 9 95 19690698 1 99 5 19690699 1 2 103 19690716 1 10 107 0 99 2 14 0 0]
+(defn f2
+  [memorystr target]
+  (let [memory (parse-memory-string memorystr)]
+    (reduce (fn [_ xy]
+              (let [result-memory (execute-2 (make-candidate memory xy) target)]
+                (when (= target (result-memory 0))
+                  (reduced xy))))
+            nil
+            (cartesian-product (range 100) (range 100)))))
+
+(f2 input 19690720) ; => [64 17]
 
 (+ (* 100 64) 17)
--- advent_of_code_2019/day05.clj
+++ advent_of_code_2019/day05.clj
@@ -1,2 +1,162 @@
-(ns day05)
+(ns day05
+  (:require [day05-input]
+            [clojure.edn :as edn]
+            [clojure.string :as string]
+            [day04]))
 
+(defn- p [x] (clojure.pprint/pprint x) x)
+
+(defn opcode
+  [instruction]
+  (mod instruction 100))
+
+(defn parameter-modes
+  [instruction]
+  (->> instruction
+       (day04/parse-to-sequence)
+       (reverse)
+       (drop 2)))
+
+(defn parameters
+  [memory pc param-count]
+  (map (fn [pos mode]
+         (if (= 1 mode)
+           (memory (+ pc (inc pos)))
+           (memory (memory (+ pc (inc pos))))))
+       (range param-count)
+       (concat (parameter-modes (memory pc))
+               (repeat 0))))
+
+(comment
+  (opcode 1002)
+  (opcode 99)
+  (opcode 3)
+  (parameters [1002 4 3 4 33] 0 2)
+  (parameter-modes 1002))
+
+(defn pc-jump-if
+  [memory pc pred]
+  (let [parameters (parameters memory pc 2)]
+    (if (pred 0 (first parameters))
+      (second parameters)
+      (+ pc 3))))
+
+(defn pred-test
+  [memory pc pred]
+  (let [parameters (parameters memory pc 2)]
+    (assoc memory
+           (memory (+ pc 3))
+           (if (pred (first parameters)
+                     (second parameters))
+             1
+             0))))
+
+(defn execute
+  "NOTE can tail call optimize... not necessary though..."
+  ([memory inputs]
+   (execute memory inputs [] 0))
+  ([memory inputs outputs pc]
+   (case (opcode (memory pc))
+     1 (execute (assoc memory
+                       (memory (+ pc 3)) ; storage parameter cannot be immediate.
+                       (apply + (parameters memory pc 2)))
+                inputs
+                outputs
+                (+ pc 4))
+     2 (execute (assoc memory
+                       (memory (+ pc 3))
+                       (apply * (parameters memory pc 2)))
+                inputs
+                outputs
+                (+ pc 4))
+     3 (execute (assoc memory
+                       (memory (+ pc 1))
+                       (first inputs))
+                (rest inputs)
+                outputs
+                (+ pc 2))
+     4 (execute memory
+                inputs
+                (apply conj outputs (parameters memory pc 1))
+                (+ pc 2))
+     5 (execute memory
+                inputs
+                outputs
+                (pc-jump-if memory pc not=))
+     6 (execute memory
+                inputs
+                outputs
+                (pc-jump-if memory pc =))
+     7 (execute (pred-test memory pc <)
+                inputs
+                outputs
+                (+ pc 4))
+     8 (execute (pred-test memory pc =)
+                inputs
+                outputs
+                (+ pc 4))
+     99 outputs
+     (throw (ex-info "bad opcode" {:x [memory pc]})))))
+
+(defn parse-memory-string
+  [memorystr]
+  (->> (string/split memorystr #",")
+       (map edn/read-string)
+       (vec)))
+
+(defn f
+  [memorystr & inputs]
+  (-> memorystr
+      (parse-memory-string)
+      (execute inputs)))
+
+#_
+(f "3,0,4,0,99" 3) ; outputs whatever it gets as inputs
+
+#_
+(f "1002,4,3,4,33")
+
+#_
+(f day05-input/input 1) ; => [0 0 0 0 0 0 0 0 0 15314507]
+;; part 1 correct
+
+
+;; PART 2
+
+(defn try-20
+  [memorystr]
+  (->> (range 20)
+       (p)
+       (map (partial f memorystr))))
+
+#_
+(try-20 "3,9,8,9,10,9,4,9,99,-1,8")
+;; => ([0] [0] [0] [0] [0] [0] [0] [0] [1] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0])
+
+#_
+(try-20 "3,9,7,9,10,9,4,9,99,-1,8")
+;; => ([1] [1] [1] [1] [1] [1] [1] [1] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0])
+
+#_
+(try-20 "3,3,1108,-1,8,3,4,3,99")
+;; => ([0] [0] [0] [0] [0] [0] [0] [0] [1] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0])
+
+#_
+(try-20 "3,3,1107,-1,8,3,4,3,99")
+;; => ([1] [1] [1] [1] [1] [1] [1] [1] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0] [0])
+
+#_
+(try-20 "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9")
+;; => ([0] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1])
+
+#_
+(try-20 "3,3,1105,-1,9,1101,0,0,12,4,12,99,1")
+;; => ([0] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1] [1])
+
+#_
+(try-20 "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99")
+;; => ([999] [999] [999] [999] [999] [999] [999] [999] [1000] [1001] [1001] [1001] [1001] [1001] [1001] [1001] [1001] [1001] [1001] [1001])
+
+#_
+(f day05-input/input 5) ; => [652726]
+;; part 2 correct
