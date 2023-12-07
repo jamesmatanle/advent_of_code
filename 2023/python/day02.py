2aab4187102c47a15db22dd2cebd16cfe1071918
jamesmatanle <jkmatanle@gmail.com>
2023-12-06 20:23:22 -0500

    day03

--- 2023/python/day02.py
+++ 2023/python/day02.py
@@ -15,3 +15,20 @@ def part2(A):
                  * max([int(x) for x in re.findall(r'(\d+) green', line)])
                  * max([int(x) for x in re.findall(r'(\d+) blue', line)]))
                 for line in A])
+
+
+TEST = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+""".strip().splitlines()
+
+IN = open('day02_input.txt').readlines()
+
+print('part1 test:', part1(TEST))  # 8
+print('part1', part1(IN))
+
+print('part2 test:', part2(TEST))  # 2286
+print('part2:', part2(IN))
--- 2023/python/day03.py
+++ 2023/python/day03.py
@@ -4,7 +4,8 @@ import re
 def part1(A):
     def has_symbol_neighbor(A, i, j):
         return any([(i+p, j+q) for p in [1, 0, -1] for q in [1, 0, -1]
-                    if -1 < i + p < M and -1 < j + q < N and A[i+p][j+q] not in '1234567890.'])
+                    if -1 < i + p < M and -1 < j + q < N
+                    and A[i+p][j+q] not in '1234567890.'])
     M, N = len(A), len(A[0])
     return sum([
         int(m.group())
@@ -15,4 +16,39 @@ def part1(A):
 
 
 def part2(A):
-    return
+    def star_neighbors(A, i, j):
+        return [(i+p, j+q) for p in [1, 0, -1] for q in [1, 0, -1]
+                if -1 < i + p < M and -1 < j + q < N and A[i+p][j+q] == '*']
+    M, N = len(A), len(A[0])
+    STARS = {}
+    for i in range(M):
+        for m in re.finditer(r'\d+', A[i]):
+            starnei = set([coord
+                           for j in range(m.start(), m.end())
+                           for coord in star_neighbors(A, i, j)])
+            for coord in starnei:
+                STARS.setdefault(coord, []).append(int(m.group()))
+    return sum(v[0]*v[1] for v in STARS.values() if len(v) == 2)
+
+
+TEST = """
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+""".strip().splitlines()
+
+
+IN = open('day03_input.txt').read().splitlines()
+
+print('part1 test:', part1(TEST))  # 4361
+print('part1:', part1(IN))
+
+print('part2 test:', part2(TEST))  # 467835
+print('part2:', part2(IN))
