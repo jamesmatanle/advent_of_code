9fe0fcb5c7772278cd8084beb7c7919ba7092641
jamesmatanle <jkmatanle@gmail.com>
2023-12-08 00:22:41 -0500

    day07 part2

--- 2023/python/day07.py
+++ 2023/python/day07.py
@@ -19,8 +19,27 @@ def part1(A):
     return sum([bid*(i+1) for i, (_, bid) in enumerate(sorted(A))])
 
 
+# PART2
+
+
+card_values2 = {c: i for i, c in enumerate('J23456789TQKA')}
+
+
+def hand_value2(hand):
+    cnt = collections.Counter(hand)
+    if cnt['J'] == 5:
+        return hand_values[(5,)]
+    cntJ = cnt.pop('J', 0)
+    cnt[cnt.most_common(1)[0][0]] += cntJ
+    return hand_values[tuple(sorted(cnt.values()))]
+
+
 def part2(A):
-    return
+    A = [x.split() for x in A]
+    A = [(tuple([hand_value2(hand)] + [card_values2[c] for c in hand]),
+          int(bid))
+         for hand, bid in A]
+    return sum([bid*(i+1) for i, (_, bid) in enumerate(sorted(A))])
 
 
 TEST = """
@@ -33,8 +52,8 @@ QQQJA 483
 
 IN = open('day07_input.txt').read().splitlines()
 
-print('part1 test:', part1(TEST))  #
+print('part1 test:', part1(TEST))  # 6440
 print('part1:', part1(IN))
 
-print('part2 test:', part2(TEST))  #
+print('part2 test:', part2(TEST))  # 5905
 print('part2:', part2(IN))
