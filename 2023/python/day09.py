0c4ac6a877d8df4519d0ff7eb6a65a5e01785e7f
jamesmatanle <jkmatanle@gmail.com>
2023-12-09 00:34:29 -0500

    day09

--- 2023/python/day09.py
+++ 2023/python/day09.py
@@ -1,26 +1,39 @@
-import math
 import re
-import itertools
+# import util
 
 
 def part1(S):
-    return
+    # @util.viz
+    def next_change(A):
+        if all(0 == x for x in A):
+            return A[-1]
+        return A[-1] + next_change([A[i] - A[i-1] for i in range(1, len(A))])
+    return sum(
+        next_change(line) for line in
+        [[int(x) for x in re.findall(r'-?\d+', A)] for A in S.splitlines()])
 
 
 def part2(S):
-    return
+    # @util.viz
+    def next_change(A):
+        if all(0 == x for x in A):
+            return A[0]
+        return A[0] - next_change([A[i] - A[i-1] for i in range(1, len(A))])
+    return sum(
+        next_change(line) for line in
+        [[int(x) for x in re.findall(r'-?\d+', A)] for A in S.splitlines()])
 
 
 TEST = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
 """.strip()
 
 IN = open('day09_input.txt').read()
 
-print('part1 test:', part1(TEST))  # =>
+print('part1 test:', part1(TEST))  # => 114
 print('part1:', part1(IN))
 
-TEST = """
-""".strip()
-
-print('part2 test:', part2(TEST))  # =>
+print('part2 test:', part2(TEST))  # => 2
 print('part2:', part2(IN))
